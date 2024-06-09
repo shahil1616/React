90658a84061c8af251499f7403eb62917af7aae6
shahil1616 <shahilkhan3303@gmail.com>
2024-06-10 02:22:33 +0530

    Episode 3

--- app.js
+++ app.js
@@ -1,25 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/* <div id="parent">
-         <div id="child">
-           <h1></h1> 
-          </div>
-</div> */
+// /* <div id="parent">
+//          <div id="child">
+//            <h1></h1>
+//           </div>
+// </div> */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I M hhh tag"),
-    React.createElement("h2", {}, "I M H2 tag"),
-  ])
-);
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World From React!"
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I M hhh tag"),
+//     React.createElement("h2", {}, "I M H2 tag"),
+//   ])
 // );
-console.log(parent); // object
+// // const heading = React.createElement(
+// //   "h1",
+// //   { id: "heading" },
+// //   "Hello World From React!"
+// // );
+// console.log(parent); // object
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+// React Element=>object=>render=>html element
+
+// const heading = React.createElement("h1", { id: "heading" }, "Namste-React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// JSX
+// React Element
+const jsxheading = <h1 id="heading">Namste React JSX</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+// Functional Component
+// Component Composition
+// const Heading = () => (
+//   <div>
+//     <Jsxheading /> <h1> Functional Component</h1>
+//   </div>
+// );
+// JS in React Component  using this --  "{}"
+// const n = 1000;
+
+// const Heading = () => (
+//   <div>
+//     <h2>{n + n}</h2>
+//     <h1> Functional Component</h1>
+//   </div>
+// );
+
+const Heading = () => (
+  <div>
+    {jsxheading}
+    <h1> Functional Component</h1>
+  </div>
+);
+
+// Same as Above can be used when there is a single of code
+
+// const Heading1 = () => <h1> Functional Component</h1>;
+root.render(<Heading />);
